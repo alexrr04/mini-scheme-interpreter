8705362df03aec3ae057ce4ecc2ff95192139ca7
alex.rodriguez.r <alex.542004@gmail.com>
2025-01-03 18:21:48 +0100

    Fix conditional expression in Collatz function for clarity

--- tests/test_files/io.scm
+++ tests/test_files/io.scm
@@ -4,7 +4,7 @@
 (define (collatz x)
     (cond ((= x 1) 1)
           ((even x) (collatz (/ x 2)))
-          (#t (collatz (+ (* 3 x) 1)))))
+          (else (collatz (+ (* 3 x) 1)))))
 
 (define (main)
     (display "Enter a number and apply the Collatz Conjecture: ")

25e09d9a364953c9769ab66039aa91374e582482
alex.rodriguez.r <alex.542004@gmail.com>
2025-01-05 13:12:49 +0100

    Add additional test cases for list operations in listOperations.scm and update expected output

--- tests/test_files/listOperations.scm
+++ tests/test_files/listOperations.scm
@@ -6,6 +6,9 @@
     (newline)
     (display (cons 0 lst)) ; (0 1 2 3 4)
     (newline)
+    (define lst2 (cons 0 (cons 1 (cons 2 (cons 3 (cons 4 '()))))))
+    (display lst2) ; (0 1 2 3 4)
+    (newline)
     (display (null? lst)) ; #f
     (newline)
     (display (null? '())) ; #t
